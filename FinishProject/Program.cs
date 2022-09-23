77a8fcaa7102252f2583b59a78ed7a6fbff9860b
Uylia Petrova <cachorro1@ya.ru>
2022-09-23 18:01:38 +0300

    Starting my project

---
+++ FinishProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            string [] array = new string [] {"now", "7", "wensday", "23"};
+            string [] NewArray = new string [array.Length];
+            CountLetters(array, NewArray);              
+        }
+    
+    static void CountLetters (string [] array, string [] NewArray)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                string word = array[i];
+                int numberOfLetters = word.ToCharArray().Count();
+                if (numberOfLetters <= 3)
+                    {
+                        NewArray [i] = word;
+                        Console.WriteLine(NewArray[i]);
+                    }
+            }  
+        }
+}
